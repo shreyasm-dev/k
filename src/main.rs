1c5524fda056ec03c9c4d6d4cfa51af8480d075a
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2023-01-17 15:59:04 -0600

    Update panic handler

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,11 @@ static TEXT: &'static str = "world";
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
   println!("Hello, {}!", TEXT);
-  println!("Goodbye, {}!", TEXT);
-
-  loop {}
+  panic!("Goodbye, {}!", TEXT);
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+  println!("{}", info);
   loop {}
 }
