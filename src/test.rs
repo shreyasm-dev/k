ddb43d711402c3ecf9e634c8e12026a11f7ebfb7
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2023-01-17 16:40:49 -0600

    More testing methods

--- src/test.rs
+++ src/test.rs
@@ -5,7 +5,16 @@ use crate::println;
 use core::fmt::Debug;
 
 #[cfg(test)]
-pub fn assert<T: PartialEq + Debug>(left: T, right: T) {
+pub fn assert(condition: bool) {
+  if condition {
+    println!("[ok]");
+  } else {
+    println!("[failed]");
+  }
+}
+
+#[cfg(test)]
+pub fn assert_eq<T: PartialEq + Debug>(left: T, right: T) {
   if left == right {
     println!("[ok]");
   } else {
