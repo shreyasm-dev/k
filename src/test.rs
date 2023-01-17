bc010191dbb653d1eca000a0b8b2bf2de7e46e2e
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2023-01-17 16:42:12 -0600

    Improve testing methods

--- src/test.rs
+++ src/test.rs
@@ -5,19 +5,23 @@ use crate::println;
 use core::fmt::Debug;
 
 #[cfg(test)]
-pub fn assert(condition: bool) {
+pub fn assert(condition: bool) -> bool {
   if condition {
     println!("[ok]");
   } else {
     println!("[failed]");
   }
+
+  condition
 }
 
 #[cfg(test)]
-pub fn assert_eq<T: PartialEq + Debug>(left: T, right: T) {
+pub fn assert_eq<T: PartialEq + Debug>(left: T, right: T) -> bool {
   if left == right {
     println!("[ok]");
   } else {
     println!("[failed] {:?} != {:?}", left, right);
   }
+
+  left == right
 }
