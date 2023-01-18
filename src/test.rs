87934f398d70a391fbeb5ccd79b2a7229cd3cb18
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2023-01-18 11:59:45 -0600

    Add test_should_not_panic

--- src/test.rs
+++ src/test.rs
@@ -50,6 +50,33 @@ macro_rules! test_should_panic {
 }
 
 #[macro_export]
+macro_rules! test_should_not_panic {
+  ($name:ident, $test:expr) => {
+    use k::{
+      qemu::{exit_qemu, QemuExitCode},
+      util::{failed, ok, running},
+    };
+
+    #[no_mangle]
+    pub extern "C" fn _start() -> ! {
+      running(stringify!($name));
+      $test();
+      ok(stringify!($name));
+      exit_qemu(QemuExitCode::Success);
+      loop {}
+    }
+
+    #[cfg(test)]
+    #[panic_handler]
+    pub fn panic(_info: &core::panic::PanicInfo) -> ! {
+      failed(stringify!($name));
+      exit_qemu(QemuExitCode::Failed);
+      loop {}
+    }
+  };
+}
+
+#[macro_export]
 macro_rules! test_no_main {
   ($name:ident, $test:expr) => {
     #[test_case]
