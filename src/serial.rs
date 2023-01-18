5449b4820a62648b79c712db87892787e66b5d1a
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2023-01-17 20:57:27 -0600

    Formatting

--- src/serial.rs
+++ src/serial.rs
@@ -21,15 +21,15 @@ pub fn _print(args: ::core::fmt::Arguments) {
 
 #[macro_export]
 macro_rules! serial_print {
-    ($($arg:tt)*) => {
-        $crate::serial::_print(format_args!($($arg)*));
-    };
+  ($($arg:tt)*) => {
+    $crate::serial::_print(format_args!($($arg)*));
+  };
 }
 
 #[macro_export]
 macro_rules! serial_println {
-    () => ($crate::serial_print!("\n"));
-    ($fmt:expr) => ($crate::serial_print!(concat!($fmt, "\n")));
-    ($fmt:expr, $($arg:tt)*) => ($crate::serial_print!(
-        concat!($fmt, "\n"), $($arg)*));
+  () => ($crate::serial_print!("\n"));
+  ($fmt:expr) => ($crate::serial_print!(concat!($fmt, "\n")));
+  ($fmt:expr, $($arg:tt)*) => ($crate::serial_print!(
+    concat!($fmt, "\n"), $($arg)*));
 }
