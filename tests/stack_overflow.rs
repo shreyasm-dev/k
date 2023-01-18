e982ba8f6e743d129adfade380a5dc3faafdf832
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2023-01-18 12:10:57 -0600

    Remove harness for stack_overflow

--- tests/stack_overflow.rs
+++ tests/stack_overflow.rs
@@ -1,8 +1,5 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(k::test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use k::test_should_panic;
 use volatile::Volatile;
