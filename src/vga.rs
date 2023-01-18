701e43dce0a4c5b13de622a982abef9606645786
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2023-01-18 09:50:37 -0600

    Formatting

--- src/vga.rs
+++ src/vga.rs
@@ -126,13 +126,13 @@ lazy_static! {
 
 #[macro_export]
 macro_rules! print {
-    ($($arg:tt)*) => ($crate::vga::_print(format_args!($($arg)*)));
+  ($($arg:tt)*) => ($crate::vga::_print(format_args!($($arg)*)));
 }
 
 #[macro_export]
 macro_rules! println {
-    () => ($crate::print!("\n"));
-    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+  () => ($crate::print!("\n"));
+  ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
 }
 
 #[doc(hidden)]
