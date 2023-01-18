cf710fec40c5c9439f65348eb14de45751177880
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2023-01-18 14:08:45 -0600

    Keyboard input

--- src/interrupt.rs
+++ src/interrupt.rs
@@ -1,8 +1,13 @@
-use crate::{gdt, println, test_no_main};
+use crate::{gdt, print, println, test_no_main};
 use lazy_static::lazy_static;
+use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
 use pic8259::ChainedPics;
 use spin;
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use spin::Mutex;
+use x86_64::{
+  instructions::port::Port,
+  structures::idt::{InterruptDescriptorTable, InterruptStackFrame},
+};
 
 pub const PIC_1_OFFSET: u8 = 32;
 pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
@@ -14,6 +19,7 @@ pub static PICS: spin::Mutex<ChainedPics> =
 #[repr(u8)]
 pub enum InterruptIndex {
   Timer = PIC_1_OFFSET,
+  Keyboard,
 }
 
 impl InterruptIndex {
@@ -39,6 +45,7 @@ lazy_static! {
     }
 
     idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
+    idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
 
     idt
   };
@@ -64,6 +71,33 @@ extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFr
     PICS
       .lock()
       .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+  }
+}
+
+extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
+  lazy_static! {
+    static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> = Mutex::new(
+      Keyboard::new(layouts::Us104Key, ScancodeSet1, HandleControl::Ignore),
+    );
+  }
+
+  let mut keyboard = KEYBOARD.lock();
+  let mut port = Port::new(0x60);
+
+  let scancode: u8 = unsafe { port.read() };
+  if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+    if let Some(key) = keyboard.process_keyevent(key_event) {
+      match key {
+        DecodedKey::Unicode(character) => print!("{}", character),
+        DecodedKey::RawKey(key) => print!("{:?}", key),
+      }
+    }
+  }
+
+  unsafe {
+    PICS
+      .lock()
+      .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
   }
 }
 
